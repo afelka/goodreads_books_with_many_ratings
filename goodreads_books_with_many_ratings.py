30059c6f3aea183202401d84608aa1fee77bb15a
afelka <erdememin@gmail.com>
2026-08-27 23:50:30 +0200

    get rid of double definition

--- goodreads_books_with_many_ratings.py
+++ goodreads_books_with_many_ratings.py
@@ -141,7 +141,7 @@ for i in range(len(goodreads_list)):
         page_count = None
 
     # Try to get my rating if it exists
-    user_rating_elem = rating_elem = driver.find_element(By.XPATH, '//div[contains(@class, "BookRatingStars")]//span[@role="group"][@aria-label]')
+    user_rating_elem = driver.find_element(By.XPATH, '//div[contains(@class, "BookRatingStars")]//span[@role="group"][@aria-label]')
     if user_rating_elem:
         user_rating_text = user_rating_elem.get_attribute("aria-label")
         match = re.search(r'Rating (\d+) out of 5', user_rating_text)
